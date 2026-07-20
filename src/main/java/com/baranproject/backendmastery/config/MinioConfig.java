f1c6a4b2b084a1c5284d33f36ceda99cc4104e45
barancil95 <barancil95@gmail.com>
2026-07-21 01:42:04 +0300

    minio config region uyumsuzluğu giderildi

--- src/main/java/com/baranproject/backendmastery/config/MinioConfig.java
+++ src/main/java/com/baranproject/backendmastery/config/MinioConfig.java
@@ -8,8 +8,10 @@ import org.springframework.context.annotation.Configuration;
 /**
  * MinIO / S3 Bağlantı Konfigürasyonu
  * 
- * @Configuration: Bu sınıfın içerisinde Spring Bean tanımları (@Bean) barındırdığını belirtir.
- * Spring ayağa kalkarken bu sınıftaki Bean metotlarını çalıştırıp dönen nesneleri IoC Container'a kaydeder.
+ * @Configuration: Bu sınıfın içerisinde Spring Bean tanımları (@Bean)
+ *                 barındırdığını belirtir.
+ *                 Spring ayağa kalkarken bu sınıftaki Bean metotlarını
+ *                 çalıştırıp dönen nesneleri IoC Container'a kaydeder.
  */
 @Configuration
 public class MinioConfig {
@@ -24,15 +26,20 @@ public class MinioConfig {
     @Value("${minio.secret-key}")
     private String secretKey;
 
+    @Value("${minio.region:eu-north-1}")
+    private String region;
+
     /**
      * MinioClient nesnesini Spring Container'a bean olarak kaydediyoruz.
-     * Bu nesne sayesinde dosya yükleme, silme gibi S3 API işlemlerini gerçekleştireceğiz.
+     * Bu nesne sayesinde dosya yükleme, silme gibi S3 API işlemlerini
+     * gerçekleştireceğiz.
      */
     @Bean
     public MinioClient minioClient() {
         return MinioClient.builder()
                 .endpoint(endpoint)
                 .credentials(accessKey, secretKey)
+                .region(region)
                 .build();
     }
 }
